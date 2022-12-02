f3abf17f804364aaab9c1561145f5dcb5fbfce1d
Beth Njuguna <libbybetha@gmail.com>
2022-12-02 15:16:44 +0300

    completed

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,12 @@
+class UsersController < ApplicationController
+
+
+    def show 
+user = User.find_by(id:  session[:user_id])
+if user
+    render json: user
+else
+    render  json:  {error:  "Not uathorized"},  status:  :unauthorized
+end
+    end 
+end
